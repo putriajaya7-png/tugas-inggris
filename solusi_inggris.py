c5f4ace0a9924592e05e5f53ebdfe3456135e417
putriajaya7-png <putriajaya7@gmail.com>
2026-08-28 22:26:40 +0700

    Update solusi_inggris.py

--- solusi_inggris.py
+++ solusi_inggris.py
@@ -24,7 +24,7 @@ def generate_with_retry(prompt, image=None):
     for kunci in kunci_acak:
         try:
             genai.configure(api_key=kunci)
-            model = genai.GenerativeModel('gemini-1.5-flash')
+            model = genai.GenerativeModel('gemini-1.5-flash-latest')
             
             if image:
                 response = model.generate_content([prompt, image])
